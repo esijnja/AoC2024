ae8b446aa85753dbc01023713c7e0d3c7742aafc
Erik Sijnja <esijnja@xs4all.nl>
2024-12-03 06:58:57 +0100

    first setup day 03 part 1

---
+++ day03/day03part1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+var pattern =@"mul\(\d{1,3}\,\d{1,3}\)";
+
+var regex = new Regex(pattern);
+
+var matches = regex.Matches(input[0]);
+var total = 0;
+foreach (Match match in matches)
+{
+    var values = match.Value.Split(",");
+    var a = int.Parse(values[0].Substring(4));
+    var b = int.Parse(values[1].Substring(0, values[1].Length - 1));
+    var result = a * b;
+    total += result;
+}
+
+Console.WriteLine(total);
