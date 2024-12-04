75a0243cd49ff4d2f745e5b1425b94869be42f2f
Erik Sijnja <e.sijnja@rijkzwaan.nl>
2024-12-04 07:43:56 +0100

    Day 4 part 2

---
+++ day04/day04part2/Program.cs
@@ -0,0 +1,34 @@
+﻿﻿var input = File.ReadAllLines("input.txt");
+
+int width = input[0].Length;
+int height = input.Length;
+
+char[,] map = new char[width,height];
+int[,] route = new int[width,height];
+
+for (int y = 0; y< input.Length; y++)
+{
+    var row = input[y].ToCharArray();
+    for (int x = 0; x < input[y].Length; x++)
+    {
+        map[x,y] = row[x];
+    }  
+}
+var count =0;
+for (int y = 1; y < height-1; y++)
+{
+    for (int x = 1; x < width-1; x++)
+    {
+        if (map[x,y] == 'A')
+        {
+            if (((map[x-1,y-1] == 'M' && map[x+1,y+1] == 'S') || 
+                (map[x-1,y-1] == 'S' && map[x+1,y+1] == 'M')) &&
+                ((map[x-1,y+1] == 'M' && map[x+1,y-1] == 'S') || 
+                (map[x-1,y+1] == 'S' && map[x+1,y-1] == 'M')))
+            {
+                count++;
+            }
+        }
+    }
+}
+Console.WriteLine(count);
