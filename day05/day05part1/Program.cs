8bfa812230813746e8cdf5b57fe0ac2d04108d05
Erik Sijnja <e.sijnja@rijkzwaan.nl>
2024-12-05 12:55:50 +0100

    day 05 first setup

---
+++ day05/day05part1/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = File.ReadAllLines("input_.txt");
+
+var totalLines = input.Length;
+int lineNumber = 0;
+var total=0;
+var rules = new List<Rule>();
+var line = string.Empty;
+do
+{
+    line = input[lineNumber++];
+    if (!string.IsNullOrEmpty(line))
+    {
+        var parts = line.Split('|');
+        var rule = new Rule(int.Parse(parts[0]), int.Parse(parts[1]));
+        rules.Add(rule);
+    }
+} while (!string.IsNullOrEmpty(line));
+
+var sequences = new List<List<int>>();
+while (lineNumber < totalLines)
+{
+    line = input[lineNumber++];
+    var sequence = line.Split(',').Select(int.Parse).ToList();
+    sequences.Add(sequence);
+}
+
+foreach (var sequence in sequences)
+{
+    
+    total += sequence.ElementAt(sequence.Count/2);
+}
+
+Console.WriteLine(total);
+
+record Rule(int before, int after);
