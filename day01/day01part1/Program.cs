cced42d103a01dbd938ebede4d8147586a1c89c4
Erik Sijnja <esijnja@xs4all.nl>
2024-12-08 16:30:41 +0100

    Day 1

---
+++ day01/day01part1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+var input = File.ReadAllLines("input.txt");
+
+var firstList = new List<int>();
+var secondList = new List<int>();
+foreach (var line in input)
+{
+   var numbers = line.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+    firstList.Add(int.Parse(numbers[0]));
+    secondList.Add(int.Parse(numbers[1]));
+}
+
+firstList.Sort();
+secondList.Sort();
+var total = 0;
+foreach (var (index, item) in firstList.Index())
+{
+    Console.WriteLine($" {index}: {item} - {secondList[index]}");;
+    var d = Math.Abs ( item - secondList[index]);
+    total += d;
+}
+
+Console.WriteLine(total);
---
+++ day01/day01part2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+var input = File.ReadAllLines("input.txt");
+
+var firstList = new List<int>();
+var secondList = new List<int>();
+foreach (var line in input)
+{
+   var numbers = line.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+    firstList.Add(int.Parse(numbers[0]));
+    secondList.Add(int.Parse(numbers[1]));
+}
+
+
+var simularity = secondList.GroupBy(x => x).Select(x=> new {x.Key, Count = x.Count()}).ToDictionary(x=>x.Key, x=>x.Count);
+var total = 0;
+foreach (var item in firstList)
+{
+//     var d = item * simularity.FirstOrDefault((key, _) => item==key)Select.Count;
+//      total += d;
+    if (simularity.ContainsKey(item))
+    {
+        total += item * simularity[item];
+    }
+}
+
+Console.WriteLine(total);
