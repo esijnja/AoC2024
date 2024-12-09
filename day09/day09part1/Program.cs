a13c9d34029c85b8da5fd8b08b6978bc1dbb2579
Erik Sijnja <e.sijnja@rijkzwaan.nl>
2024-12-09 08:33:44 +0100

    Day 9 part 1 alomst there

---
+++ day09/day09part1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+var input = File.ReadAllLines("input_.txt");
+
+
+var encryptedDisk = input[0].ToCharArray();
+var diskSb = new StringBuilder();
+
+
+for (int i = 0; i < encryptedDisk.Length; i++)
+{
+    if (i%2 == 0)
+    {
+        for (int j = 0; j < encryptedDisk[i]; j++)
+        {
+            diskSb.Append(i);
+        }
+        
+    }
+    else
+    {
+        for (int j = 0; j < encryptedDisk[i]; j++)
+        {
+            diskSb.Append('.');
+        }
+    }
+}
+
+var disk = diskSb.ToString();
+var disk2 = disk.Replace(".", string.Empty);
+Console.WriteLine(disk2);
+
+var diskChars = disk2.ToCharArray();
+var total = 0L;
+for (long i = 0; i < diskChars.Length; i++)
+{
+    total += (long.Parse($"{diskChars[i]}") * i);
+}
+
+Console.WriteLine(total);
