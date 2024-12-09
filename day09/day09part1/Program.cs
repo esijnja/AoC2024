cd9c7eda8331d4ce5b1f7bf61630a94546863bbe
Erik Sijnja <e.sijnja@rijkzwaan.nl>
2024-12-09 16:28:08 +0100

    Step closer

--- day09/day09part1/Program.cs
+++ day09/day09part1/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 var input = File.ReadAllLines("input_.txt");
 
 
-var encryptedDisk = input[0].ToCharArray();
+var encryptedDisk = input[0].ToCharArray().Select(c => int.Parse($"{c}")).ToArray();
 var diskSb = new StringBuilder();
 
 
@@ -12,11 +12,11 @@ for (int i = 0; i < encryptedDisk.Length; i++)
 {
     if (i%2 == 0)
     {
+        Console.WriteLine($"{i} {encryptedDisk[i]}");
         for (int j = 0; j < encryptedDisk[i]; j++)
         {
-            diskSb.Append(i);
-        }
-        
+            diskSb.Append(i/2);
+        }  
     }
     else
     {
@@ -26,16 +26,35 @@ for (int i = 0; i < encryptedDisk.Length; i++)
         }
     }
 }
-
+Console.WriteLine(input[0]);
 var disk = diskSb.ToString();
-var disk2 = disk.Replace(".", string.Empty);
+Console.WriteLine(disk);
+int jj = 0;
+var disk2 = disk.ToCharArray();
+for (int i = disk2.Length-1; i >= 0; i--)
+{
+    if (disk2[i] != '.')
+    {
+        for (; jj < disk2.Length; jj++)
+        {
+            if (disk2[jj] == '.')
+            {
+                disk2[jj] = disk2[i];
+                disk2[i] = '.';
+                break;
+            }            
+        }
+    }
+    Console.WriteLine(string.Join("", disk2)); 
+}
+
+//var disk2 = disk.Replace(".", string.Empty);
 Console.WriteLine(disk2);
 
-var diskChars = disk2.ToCharArray();
 var total = 0L;
-for (long i = 0; i < diskChars.Length; i++)
+for (long i = 0; i < disk2.Length; i++)
 {
-    total += (long.Parse($"{diskChars[i]}") * i);
+    if (disk2[i] != '.') total += (long.Parse($"{disk2[i]}") * i);
 }
 
 Console.WriteLine(total);
