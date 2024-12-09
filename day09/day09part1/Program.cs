35ec55f1311857012135b414a6f70b83a071320b
Erik Sijnja <esijnja@xs4all.nl>
2024-12-09 17:54:46 +0100

    Day 09 part1

--- day09/day09part1/Program.cs
+++ day09/day09part1/Program.cs
@@ -7,6 +7,9 @@ var input = File.ReadAllLines("input.txt");
 var encryptedDisk = input[0].ToCharArray().Select(c => int.Parse($"{c}")).ToArray();
 var diskSb = new StringBuilder();
 
+var diskLength = encryptedDisk.Length/2;
+
+var diskInts = new List<int>();
 
 for (int i = 0; i < encryptedDisk.Length; i++)
 {
@@ -15,32 +18,34 @@ for (int i = 0; i < encryptedDisk.Length; i++)
         Console.WriteLine($"{i} {encryptedDisk[i]}");
         for (int j = 0; j < encryptedDisk[i]; j++)
         {
-            diskSb.Append(i/2);
+            diskInts.Add(i/2); 
+            //diskSb.Append(i/2);
         }  
     }
     else
     {
         for (int j = 0; j < encryptedDisk[i]; j++)
         {
-            diskSb.Append('.');
+            diskInts.Add(-1);
+            //diskSb.Append('.');
         }
     }
 }
 Console.WriteLine(input[0]);
-var disk = diskSb.ToString();
-Console.WriteLine(disk);
+//var disk = diskSb.ToString();
+//Console.WriteLine(disk);
 int jj = 0;
-var disk2 = disk.ToCharArray();
+var disk2 = diskInts.ToArray();
 for (int i = disk2.Length-1; i >= 0; i--)
 {
-    if (disk2[i] != '.')
+    if (disk2[i] != -1)
     {
         for (; jj < disk2.Length && i>jj; jj++)
         {
-            if (disk2[jj] == '.')
+            if (disk2[jj] == -1)
             {
                 disk2[jj] = disk2[i];
-                disk2[i] = '.';
+                disk2[i] = -1;
                 break;
             }            
         }
@@ -49,12 +54,12 @@ for (int i = disk2.Length-1; i >= 0; i--)
 }
 
 //var disk2 = disk.Replace(".", string.Empty);
-Console.WriteLine(disk2);
+//Console.WriteLine(disk2);
 
 var total = 0L;
 for (long i = 0; i < disk2.Length; i++)
 {
-    if (disk2[i] == '.') 
+    if (disk2[i] == -1) 
         break;
     total += (long.Parse($"{disk2[i]}") * i);
 }
