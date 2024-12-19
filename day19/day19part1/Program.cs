1df1d950fbb602b31d111418256868a9167d04bd
Erik Sijnja <esijnja@xs4all.nl>
2024-12-19 06:59:00 +0100

    day 19 part 1 setup

---
+++ day19/day19part1/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = File.ReadAllLines("input_.txt");
+
+var towel = input[0].Split(" ");
+
+var patterns = new List<string>();
+
+for (int i = 2; i < input.Length; i++)
+{
+    patterns.Add(input[i]);
+}
+
+foreach (var pattern in patterns)
+{
+    
+
+}
